b96cc20ff9821bbd6a40960a7e4db5b5d2d1f50e
Casey DeBuse <75276126+cjdebuse@users.noreply.github.com>
2024-04-02 18:14:42 -0400

    Implement operand construction

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let operand1;
-let operand2;
-let operator;
+const body = document.querySelector('body');
+const display = body.querySelector('.display-area > p');
+const inputs = body.querySelector('.inputs');
+
+inputs.addEventListener('click', inputClicked);
+
+let expression = {
+    operand1: '0',
+    operand2: '',
+    operator: ''
+};
 
 const operations = {
     calculate: function (a, o , b) {
@@ -23,3 +31,42 @@ const operations = {
         return a / b;
     }
 };
+
+function inputClicked (event) {
+    switch (event.target.className) {
+        case 'number':
+            buildOperand(event.target.textContent);
+            break;
+        case 'operator':
+            break;
+    }
+}
+
+function buildOperand (val) {
+    let operand = '';
+
+    if (!expression['operator']) {
+        operand = 'operand1';
+    }
+
+    switch (val) {
+        case '.':
+            if (!expression[operand].includes('.')) {
+                expression[operand] += '.';
+            }
+            break;
+        case '±':
+            if (!expression[operand].startsWith('-')) {
+                expression[operand] = expression[operand].padStart(expression[operand].length + 1, '-');
+            } else {
+                expression[operand] = expression[operand].slice(1);
+            }
+            break;
+        default:
+            expression[operand] = 
+                expression[operand] === '0' ? val : 
+                expression[operand] + val;
+    }
+
+    display.textContent = expression[operand];
+}
